e182d858e4031055c4cb4cd89d0500a77b2f6066
afirthes <afirthes@gmail.com>
2021-07-11 09:38:12 +0300

    Initial Commit

---
+++ obj-c-sqllite/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  obj-c-sqllite
+//
+//  Created by sehio on 11.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
